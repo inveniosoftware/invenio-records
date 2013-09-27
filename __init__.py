7d672c610419549818c5dd0b4e0b3ea69f690112
Jiri Kuncar <jiri.kuncar@cern.ch>
2013-09-27 15:01:56 +0200

    global: new code structure
    * Adds Sphinx documentation.
    
    * Moves Jinja2 templates to the new code structure
      `invenio/modules/<name>/templates/<name>/index.html`.
    
    * Adds new tag module developed by Krzysztof Lis
      <krzysztof.lis@cern.ch>.
    
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Co-authored-by: Esteban J. G. Gabancho <esteban.gabancho@cern.ch>
    Co-authored-by: Krzysztof Lis <krzysztof.lis@cern.ch>
    Co-authored-by: Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
    
    NOTE: beware, patch amended by Tibor to use new file names

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-{#
+# -*- coding: utf-8 -*-
+##
 ## This file is part of Invenio.
-## Copyright (C) 2012 CERN.
+## Copyright (C) 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -15,15 +16,3 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-#}
-{%- if not request.is_xhr -%}
-  {% extends "records/base.html" %}
-{%- endif -%}
-
-{% block record_content %}
-  <div class="page-header">
-    {{ format_record(recid, 'hs', ln=g.ln)|safe }}
-  </div>
-
-  {{ format_record(recid, 'HDFILE', ln=g.ln)|safe }}
-{% endblock %}
