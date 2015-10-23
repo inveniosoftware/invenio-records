606c0f8ee2395fcdcfb1824e9996ed5197b66d24
Javier Martin Montull <javier.martin.montull@cern.ch>
2015-10-23 11:06:50 +0200

    tasks: multiple index support
    * Adapts record indexing tasks to support different ElasticSearch
      indices.
    
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- invenio_records/tasks/index.py
+++ invenio_records/tasks/index.py
@@ -17,15 +17,31 @@
 # along with Invenio; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import six
+
+from invenio.base.globals import cfg
 from invenio_celery import celery
 from invenio_ext.es import es
+from invenio_search.api import Query
+from invenio_search.walkers.elasticsearch import ElasticSearchDSL
+
+
+def get_record_index(record):
+    """Decide which index the record should go to."""
+    query = 'collection:"{collection}"'
+    for collection, index in six.iteritems(
+        cfg["SEARCH_ELASTIC_COLLECTION_INDEX_MAPPING"]
+    ):
+        if Query(query.format(collection=collection)).match(record):
+            return index
 
 
 @celery.task
 def index_record(recid, json):
     """Index a record in elasticsearch."""
+    index = get_record_index(json) or cfg['SEARCH_ELASTIC_DEFAULT_INDEX']
     es.index(
-        index='records',
+        index=index,
         doc_type='record',
         body=json,
         id=recid
@@ -35,11 +51,12 @@ def index_record(recid, json):
 @celery.task
 def index_collection_percolator(name, dbquery):
     """Create an elasticsearch percolator for a given query."""
-    from invenio_search.api import Query
-    from invenio_search.walkers.elasticsearch import ElasticSearchDSL
-    es.index(
-        index='records',
-        doc_type='.percolator',
-        body={'query': Query(dbquery).query.accept(ElasticSearchDSL())},
-        id=name
-    )
+    indices = set(cfg["SEARCH_ELASTIC_COLLECTION_INDEX_MAPPING"].values())
+    indices.add(cfg['SEARCH_ELASTIC_DEFAULT_INDEX'])
+    for index in indices:
+        es.index(
+            index=index,
+            doc_type='.percolator',
+            body={'query': Query(dbquery).query.accept(ElasticSearchDSL())},
+            id=name
+        )
