d2b27a46311356898d1a6661ec7d29056f306e13
Javier Martin Montull <javier.martin.montull@cern.ch>
2017-06-15 14:52:15 +0200

    models: move json column to jsonb in postgresql

---
+++ invenio_records/alembic/07fb52561c5c_alter_column_from_json_to_jsonb.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2017 CERN.
+#
+# Invenio is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+#
+# In applying this license, CERN does not
+# waive the privileges and immunities granted to it by virtue of its status
+# as an Intergovernmental Organization or submit itself to any jurisdiction.
+
+"""Alter column from json to jsonb."""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '07fb52561c5c'
+down_revision = '862037093962'
+branch_labels = ()
+depends_on = None
+
+
+def upgrade():
+    """Upgrade database."""
+    if op._proxy.migration_context.dialect.name == 'postgresql':
+        op.alter_column('records_metadata', 'json', type_=sa.dialects.postgresql.JSONB, postgresql_using='json::text::jsonb')
+
+
+def downgrade():
+    """Downgrade database."""
+    if op._proxy.migration_context.dialect.name == 'postgresql':
+        op.alter_column('records_metadata', 'json', type_=sa.dialects.postgresql.JSON, postgresql_using='json::text::json')
