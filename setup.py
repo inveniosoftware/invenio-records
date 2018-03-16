1e378560920789dbd4d3d679213b67d5545bb457
Invenio <info@inveniosoftware.org>
2018-03-16 15:42:24 +0100

    installation: removed pytest-cache dependency

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ tests_require = [
     'isort>=4.3.0',
     'mock>=1.0.0',
     'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
     'pytest>=3.2.3,!=3.3.0',
