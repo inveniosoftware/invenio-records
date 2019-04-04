fe23e440ac38575c049310e051549fca8c4d509d
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2019-04-04 16:56:50 +0200

    docs: optimistic concurrency control

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Invenio-Records'
-copyright = u'2015, CERN'
+copyright = u'2015-2019, CERN'
 author = u'CERN'
 
 # The version info for the project you're documenting, acts as replacement for
