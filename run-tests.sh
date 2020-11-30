897dcb3b8e212acee8860a6fd44789434174c00e
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-11-30 17:22:51 +0100

    global: migrate CI to GH Actions
    * Closes #235.
    
    Co-authored-by: ParthS007 <parth.shandilya@cern.ch>

--- run-tests.sh
+++ run-tests.sh
@@ -10,8 +10,14 @@
 # Usage:
 #   env DB=postgresql ./run-tests.sh
 
-python -m check_manifest --ignore ".travis-*" && \
-python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
 docker-services-cli up ${DB}
 python -m pytest
 tests_exit_code=$?
