bf3f2d04ccd23b647577f6fad7b178e32ee72e86
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-12-02 11:23:11 +0100

    tests: always bring down docker services
    * Fixes an issue where if a test failed, then the docker services
      would not be destroyed.

--- run-tests.sh
+++ run-tests.sh
@@ -16,10 +16,15 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
+# Always bring down services
+function cleanup {
+  docker-services-cli down
+}
+trap cleanup EXIT
+
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 docker-services-cli up ${DB}
 python -m pytest
 tests_exit_code=$?
-docker-services-cli down
 exit "$tests_exit_code"
