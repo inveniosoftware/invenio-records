edd2d29d3cbd7f4f2ab6d3517e03369971ff263c
Diego Rodriguez <diego.rodriguez@cern.ch>
2021-03-31 08:33:17 +0200

    ext: configurable local ref resolver and store
    * Adds new configuration variables to inject a custom JSONSchema RefResolver
      together with a custom store
      (addresses inveniosoftware/invenio-rdm-records#/356).
    
    * Passes a custom `urljoin_cache` to allow JSONSchema scope resolutions
      with custom URI schemes, which is currently not possible (see more
      [here](https://github.com/Julian/jsonschema/issues/649)). JSONSchema
      scope resolution uses `urllib.parse.urljoin` which is only compatible
      with a set of well known schemes, the applied solution has been
      suggested [here](https://bugs.python.org/issue18828).
    
      Regarding the behaviour, any custom scheme will be registered and
      accepted because the concrete custom scheme is not known by
      InvenioRefResolver at creation time.

--- invenio_records/ext.py
+++ invenio_records/ext.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2015-2021 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,11 +9,16 @@
 """Invenio module for metadata storage."""
 
 
+from invenio_base.utils import obj_or_import_string
 from jsonref import JsonRef
 from jsonresolver import JSONResolver
 from jsonresolver.contrib.jsonref import json_loader_factory
 from jsonresolver.contrib.jsonschema import ref_resolver_factory
 from jsonschema import validate
+from jsonschema.compat import lru_cache
+
+from invenio_records.errors import RecordsRefResolverConfigError
+from invenio_records.resolver import urljoin_with_custom_scheme
 
 from . import config
 
@@ -25,17 +30,34 @@ class _RecordsState(object):
         """Initialize state."""
         self.app = app
         self.resolver = JSONResolver(entry_point_group=entry_point_group)
-        self.ref_resolver_cls = ref_resolver_factory(self.resolver)
+        self.refresolver_cls = ref_resolver_factory(self.resolver)
+        self.refresolver_store = None
+        if (
+            self.app.config.get("RECORDS_REFRESOLVER_CLS")
+        ):
+            self.refresolver_cls = obj_or_import_string(
+                self.app.config.get("RECORDS_REFRESOLVER_CLS"),
+            )
+            self.refresolver_store = obj_or_import_string(
+                self.app.config.get("RECORDS_REFRESOLVER_STORE")
+            )
+
         self.loader_cls = json_loader_factory(self.resolver)
 
     def validate(self, data, schema, **kwargs):
         """Validate data using schema with ``JSONResolver``."""
         if not isinstance(schema, dict):
             schema = {'$ref': schema}
+        refresolver_cls_kwargs = {}
+        if self.refresolver_store:
+            refresolver_cls_kwargs['store'] = self.refresolver_store
+            refresolver_cls_kwargs['urljoin_cache'] = \
+                lru_cache(1024)(urljoin_with_custom_scheme)
         return validate(
             data,
             schema,
-            resolver=self.ref_resolver_cls.from_schema(schema),
+            resolver=self.refresolver_cls.from_schema(
+                schema, **refresolver_cls_kwargs),
             types=self.app.config.get('RECORDS_VALIDATION_TYPES', {}),
             **kwargs
         )
@@ -75,3 +97,12 @@ class InvenioRecords(object):
         for k in dir(config):
             if k.startswith('RECORDS_'):
                 app.config.setdefault(k, getattr(config, k))
+
+        if (
+            app.config.get("RECORDS_REFRESOLVER_CLS")
+            and not app.config.get("RECORDS_REFRESOLVER_STORE")
+        ):
+            raise RecordsRefResolverConfigError(
+                "RECORDS_REFRESOLVER_CLS config requires "
+                "RECORDS_REFRESOLVER_STORE to be set."
+            )
---
+++ invenio_records/resolver.py
@@ -0,0 +1,44 @@
+
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2021 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""InvenioRefResolver."""
+
+import urllib.parse
+
+from jsonschema import RefResolutionError, RefResolver
+from jsonschema.compat import urljoin
+
+
+class InvenioRefResolver(RefResolver):
+    """Local Invenio JSONSchemas ref resolver class."""
+
+    def resolve_remote(self, uri):
+        """Block remote ref resolve."""
+        raise RefResolutionError(
+            "{uri} not found in local registry.".format(uri=uri))
+
+
+def urljoin_with_custom_scheme(*args, **kwargs):
+    """Patch urljoin call when using local store with custom schemes.
+
+    Allows using custom schemes by extending urllib.parse
+    configuration (work-around suggested in
+    https://bugs.python.org/issue18828). This patch won't
+    be needed once https://github.com/Julian/jsonschema/issues/649
+    gets fixed.
+    """
+    for arg in args:
+        if isinstance(arg, str) and "://" in arg:
+            scheme = arg.split("://")[0]
+            if scheme not in urllib.parse.uses_relative:
+                urllib.parse.uses_relative.append(scheme)
+            if scheme not in urllib.parse.uses_netloc:
+                urllib.parse.uses_netloc.append(scheme)
+
+    return urljoin(*args, **kwargs)
