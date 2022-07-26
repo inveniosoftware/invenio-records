3e47c47f5e7950f6a9fac7a8d8753da1f448cfa9
Pablo Panero <pablo.panero@cern.ch>
2022-07-26 16:42:50 +0200

    systemfields: add multi relations field

--- invenio_records/systemfields/relations/field.py
+++ invenio_records/systemfields/relations/field.py
@@ -74,3 +74,80 @@ class RelationsField(SystemField):
         """Initialise the model field."""
         self.obj(record).validate()
         self.obj(record).clean()
+
+
+class MultiRelationsField(RelationsField):
+    """Relations field for connections to external entities."""
+
+    def __init__(self, **fields):
+        """Initialize the field."""
+        assert all(
+            isinstance(f, RelationBase) or isinstance(f, RelationsField)
+            for f in fields.values()
+        )
+        self._fields = {
+            key: field
+            for (key, field) in fields.items()
+            if isinstance(field, RelationBase)
+        }
+        self._relation_fields = {
+            key: field
+            for (key, field) in fields.items()
+            if isinstance(field, RelationsField)
+        }
+
+    def __getattr__(self, name):
+        """Get a field definition."""
+        if name in self._fields:
+            return self._fields[name]
+
+        raise AttributeError
+
+    def __iter__(self):
+        """Iterate over the configured fields."""
+        return iter(getattr(self, f) for f in self._fields)
+
+    def __contains__(self, name):
+        """Return if a field exists in the configured fields."""
+        return name in self._fields
+
+    #
+    # Helpers
+    #
+    def obj(self, instance):
+        """Get the relations object."""
+        # Check cache
+        obj = self._get_cache(instance)
+        if obj:
+            return obj
+        for relation_field in self._relation_fields.values():
+            for (name, field) in relation_field._fields.items():
+                if name not in self._fields:
+                    self._fields[name] = field
+        obj = RelationsMapping(record=instance, fields=self._fields)
+        self._set_cache(instance, obj)
+        return obj
+
+    #
+    # Data descriptor
+    #
+    def __get__(self, record, owner=None):
+        """Accessing the attribute."""
+        # Class access
+        if record is None:
+            return self
+        return self.obj(record)
+
+    def __set__(self, instance, values):
+        """Setting the attribute."""
+        obj = self.obj(instance)
+        for k, v in values.items():
+            setattr(obj, k, v)
+
+    #
+    # Record extension
+    #
+    def pre_commit(self, record):
+        """Initialise the model field."""
+        self.obj(record).validate()
+        self.obj(record).clean()
