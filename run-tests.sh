c47c490c644aa259c90509e7b81950ba87d6a306
David <72449192+rekt-hard@users.noreply.github.com>
2023-07-04 09:15:16 +0200

    transifex: update config (#309)
    ci: use i18n reusable workflows

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2020 CERN.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2023 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
