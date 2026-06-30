551a712c84451d21fc72c338417bef103cdd2d5b
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-06-30 10:44:27 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * translate the i18n/translation mechanisms as well
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2015-2022 CERN.
-# SPDX-FileCopyrightText: 2021 TU Wien.
-# SPDX-License-Identifier: MIT
-
-"""Invenio-Records is a metadata storage module."""
-
-from setuptools import setup
-
-setup()
